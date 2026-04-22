1d8f7693073a2d444bc019781d5295e1f678023b
CallemeAK09 <mr.ak11718@gmail.com>
2026-04-22 19:45:34 +0530

    Html enhanced.

--- orm_compiler/settings.py
+++ orm_compiler/settings.py
@@ -95,6 +95,17 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
